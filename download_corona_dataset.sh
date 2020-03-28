9a89a2b379af526c1b36ab0a936a05221f684124
YeonwooSung <neos960518@gmail.com>
2020-03-28 15:14:30 +0900

    Add script for downloading the dataset

---
+++ download_corona_dataset.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# download covid19 dataset from the github repository
+git clone https://github.com/CSSEGISandData/COVID-19.git
+
+echo "Successfully cloned the dataset from github repository"
+echo "Finish download process"
